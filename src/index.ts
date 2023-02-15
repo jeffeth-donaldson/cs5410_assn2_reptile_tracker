25b98653efb7a8994920c2c381079b45d28c104a
Joshua McClung <josh.mcclung66@gmail.com>
2023-02-15 12:28:50 -0700

    added auth

--- src/index.ts
+++ src/index.ts
@@ -1,31 +1,83 @@
-import express from "express";
+import express, {Request, RequestHandler} from "express";
 import { PrismaClient } from "@prisma/client";
+import bcrypt from "bcrypt";
+import dotenv from "dotenv";
+import jwt from "jsonwebtoken";
 
+dotenv.config();
 const client = new PrismaClient();
 const app = express();
 app.use(express.json());
 
+type JWTBody = {
+  userId: number,
+}
+
+type RequestWithJWTBody = Request & {
+  jwtBody?: JWTBody
+}
+const authenticationMiddleware: RequestHandler = async (req: RequestWithJWTBody, res, next) => {
+  const token = req.headers.authorization?.split(" ")[1];
+  try {
+    const jwtBody = jwt.verify(token || '', process.env.ENCRYPTION_KEY!!) as JWTBody;
+    req.jwtBody = jwtBody;
+  } catch (error) {
+    console.log("token failed validation")
+  } finally {
+    next();
+  }
+}
+
+type CreateUserBody = {
+  email: string,
+  password: string,
+  firstName: string,
+  lastName: string,
+}
 app.post('/users', async (req, res) => {
+  const {email, password, firstName, lastName} = req.body as CreateUserBody;
+  const passwordHash = await bcrypt.hash(req.body.password, 10)
   const user = await client.user.create({
     data: {
-      firstName: "Joseph",
-      lastName: "Ditton",
-      email: "joseph.ditton@usu.edu",
-      passwordHash: "q23oejklnvzlskjfdnf"
+      firstName,
+      lastName,
+      email,
+      passwordHash
     }
   });
   res.json({ user });
 });
 
-app.get("/users", async (req, res) => {
-  const users = await client.user.findMany();
-  res.json({ users });
-})
-
-app.get("/", (req, res) => {
-  res.send(`<h1>Hello, world!</h1>`);
+type LoginBody = {
+  email: string,
+  password: string
+}
+app.post('/token', async (req, res) => {
+  const {email, password} = req.body as LoginBody;
+  const user = await client.user.findFirst({
+    where: {
+      email,
+    }
+  })
+  if (!user) {
+    res.status(404).json({message: "invalid email or password"})
+    return;
+  }
+  const isValid = await bcrypt.compare(password, user.passwordHash)
+  if (!isValid) {
+    res.status(404).json({message: "invalid email or password"})
+    return;
+  }
+  const token = jwt.sign({
+    userId: user.id
+  },
+  process.env.ENCRYPTION_KEY!!,
+  {
+    expiresIn: '2m'
+  })
+  res.status(200).json({user, token});
 });
-
-app.listen(3000, () => {
-  console.log("I got started!");
+const port = parseInt(process.env.PORT??'3000')
+app.listen(port, () => {
+  console.log(`Listening on port:${port}`);
 });
